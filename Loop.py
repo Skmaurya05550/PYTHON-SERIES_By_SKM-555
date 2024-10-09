c9f30ffc42a2373074947c31e20a382cff44f6e7
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 22:23:56 +0530

    Create Loop.py

---
+++ Loop.py
@@ -0,0 +1,12 @@
+# For loop
+# Explanation : Loops are used for iterating over a sequence .
+
+
+for i in range(5):
+    print(i)
+
+# While loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
