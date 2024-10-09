2886f2de454afc676e7cdbe7d7ad3de5d1df5f7e
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 22:04:12 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,9 @@
+# Arithmetic operators
+# Explanation : Operators are special symbols that perform operation on variables and values .
+
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
