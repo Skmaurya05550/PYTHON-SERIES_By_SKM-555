b5cee86c14e0aefa8a65d5589d17d44e9683a908
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 22:49:11 +0530

    Create OOP.py

---
+++ OOP.py
@@ -0,0 +1,23 @@
+# Class and object
+# Explanation : OOP is a programming paradigm based on the concept of objects .
+
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        return "Woof!"
+
+my_dog = Dog("Buddy")
+print(my_dog.bark())
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        return "Woof!"
+
+my_dog = Dog("Buddy")
+print(my_dog.bark())
