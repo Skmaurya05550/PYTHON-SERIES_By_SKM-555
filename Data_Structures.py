30e141331f39fcf7ec0669cee39c162f66009e81
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 22:39:00 +0530

    Create Data_Structures.py

---
+++ Data_Structures.py
@@ -0,0 +1,10 @@
+# Lists
+# Explanation : Python has build-in data structures like lists, tuples, sets and dictionaries .
+
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+
+# Dictionary
+person = {"name": "Alice", "age": 25}
+print(person["name"])
