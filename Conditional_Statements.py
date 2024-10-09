88c16c7b58ad660f81daebb3eeae3cb34be95c75
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 22:21:42 +0530

    Create Conditional_Statements.py

---
+++ Conditional_Statements.py
@@ -0,0 +1,10 @@
+# Conditional statements
+# Explanation : Conditional statements allow us to execute certain pieces of code based on condition .
+
+num = 10
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
