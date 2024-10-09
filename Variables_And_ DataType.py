a408b7d6e1308de8ee2b91fe0ffa279383dc8de4
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 22:08:04 +0530

    Update Variables_And_ DataType.py

--- Variables_And_ DataType.py
+++ Variables_And_ DataType.py
@@ -1,5 +1,7 @@
 # Variables and data types
-name = "Alice"  # String
+# Explanation : Variables are used to store data, and  Python has several build-in data types .
+
+#name = "Alice"  # String
 age = 25        # Integer
 height = 5.5    # Float
 is_student = True  # Boolean
